fd3bce75d89c91655e925e20d720990c1ab8f614
Leonardo Luz Almeida <leoluz@users.noreply.github.com>
2021-11-19 16:05:38 -0500

    Clean up

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -12,13 +12,6 @@ g.dashboard_custom_header = {
 "  ╚═════╝  ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝",
 }
 
---'last_session'] = ' '
---'find_history'] = ' '
---'find_file'] = ' '
---'new_file'] = ' '
---'find_word'] = ' '
---'book_marks'] = ' '
-
 map("n", "<leader>fr", ":DashboardFindHistory<CR>", silent)
 map("n", "<leader>fl", ":SessionLoad<CR>", silent)
 map("n", "<leader>fn", ":DashboardNewFile<CR>", silent)
