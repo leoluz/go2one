5f5e97a1b4411fc4614049c5e57ed6e029bd873f
LeoLuz <leoluz@users.noreply.github.com>
2023-08-21 20:40:08 -0400

    Clean up old packer files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,10 @@
-require("plugins-lazy")
+require("plugins")
 require("settings")
 require("mappings")
 require("autocmd")
 
 -- local gotest = require "go2one.gotest"
--- 
+--
 -- gotest.setup {
 --   results_handler = "telescope",
 -- }
--- nvim/lua/plugins-old/fugitive.lua
+++
@@ -1,6 +0,0 @@
-local map = require("utils").map
-local silent = { silent = true }
-
-map("n", "<leader>gg", ":Git<CR>", silent)
-map("n", "<leader>gc", ":Git commit -s<CR>", silent)
-map("n", "<leader>gp", ":Git push<CR>", silent)
