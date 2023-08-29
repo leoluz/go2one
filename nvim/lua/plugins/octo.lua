4dc191aaad4c981dfacb1d531c459efdaabf7a88
Leonardo Luz Almeida <leoluz@users.noreply.github.com>
2023-08-29 15:22:52 -0400

    Add Octo for code review

---
+++ nvim/lua/plugins/octo.lua
@@ -0,0 +1,11 @@
+return {
+  "pwntester/octo.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope.nvim" },
+    { "nvim-tree/nvim-web-devicons" },
+  },
+  config = function()
+    require "octo".setup()
+  end
+}
